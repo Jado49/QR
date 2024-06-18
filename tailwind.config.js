38cdf459080523f4eedd5f56ce42bd10dd3dcdd4
Jado49 <mic2016mic2016@gmail.com>
2024-06-18 17:54:48 +0100

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        // Customize your colors here
+        // Example:
+        // 'primary': '#3490dc',
+        // 'secondary': '#ffed4a',
+        // 'danger': '#e3342f',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

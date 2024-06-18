99bc22453c286d013ec95d3f55103dfe20cf025a
Jado49 <mic2016mic2016@gmail.com>
2024-06-18 17:57:35 +0100

    postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -6,3 +6,17 @@ module.exports = {
     autoprefixer: {},
   },
 };
+// Example postcss.config.js with additional plugins
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Add more plugins as needed
+    // Example:
+    // require('@fullhuman/postcss-purgecss')({
+    //   content: ['./src/**/*.html', './src/**/*.js'],
+    //   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    // }),
+  ],
+};
